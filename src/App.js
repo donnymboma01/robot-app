b5464d3bb0c76159b80246ee6d134d6ecd147824
Donny Mboma <donnymboma01@gmail.com>
2021-06-10 11:46:40 +0200

    SearchBox.js search works

--- src/App.js
+++ src/App.js
@@ -15,18 +15,20 @@ class App extends Component {
 
     onSearchChange (event){
         this.setState({ searchField : event.target.value })
-        const filterRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-        })
-        console.log(filterRobots);
+        //console.log(filterRobots);
     }
 
     render(){
+        //ES6 javascript
+        const filterRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+
         return (
             <div className="tc">
                 <h1>Robot friends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={this.state.robots} />
+                <CardList robots={filterRobots} />
             </div>
         );
     }
