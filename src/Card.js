b721872ffab3b992f60d858691dd415603f5cdaf
Donny Mboma <donnymboma01@gmail.com>
2021-06-10 10:26:19 +0200

    robots

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Card = () =>{
+const Card = (props) =>{
+
+    const { name,email } = props;
     return(
         <div className='bg-light-green dib pa3 ma2 grow bw2 shadow-5'>
             <img src="https://robohash.org/test?200x200" alt="robots"/>
             <div>
-                <h2>John Doe</h2>
-                <p>johndoe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
