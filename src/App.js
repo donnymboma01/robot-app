f76022665b4e8189084e1a68fd9cc35c537c3e4e
Donny Mboma <donnymboma01@gmail.com>
2021-06-10 11:13:01 +0200

    SearchBox.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import CardList from "./CardList";
+import SearchBox from "./SearchBox";
 
 function App() {
   return (
-    <div className="App">
+    <div className="tc">
+        <h1>Robot friends</h1>
+        <SearchBox />
       <CardList />
     </div>
   );
---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = ()=>{
+    return(
+        <div>
+            <input
+                className='pa3 b--green bg-lightest-blue'
+                type="text"
+                placeholder="Search robots"
+            />
+        </div>
+    )
+}
+
+export default SearchBox;
